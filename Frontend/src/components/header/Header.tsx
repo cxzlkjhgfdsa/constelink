585b49c38fd4105c9fb987d6d2deb34bf65f6a33
정원철 <gksrud316@naver.com>
2023-03-13 01:35:38 +0900

    :lipstick: [FE] 헤더 UI Selected 효과추가
    - 헤더 display 방식 교체
    - Selected 효과 추가

--- Frontend/src/components/header/Header.tsx
+++ Frontend/src/components/header/Header.tsx
@@ -1,21 +1,65 @@
+import { useEffect, useState } from "react";
 import styles from "./Header.module.css";
 
 const Header: React.FC = () => {
+    const [selectedMenu, setSelectedMenu] = useState(localStorage.getItem('selectedMenu') || 'default');
 
+    useEffect(() => {
+  
+        const path = window.location.pathname;
+        switch (path) {
+            case '/notice':
+                setSelectedMenu('공지사항');
+                break;
+            case '/board':
+                setSelectedMenu('게시판');
+                break;
+            case '/trade':
+                setSelectedMenu('거래정보');
+                break;
+            default:
+                setSelectedMenu('default');
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('selectedMenu', selectedMenu);
+    }, [selectedMenu]);
+
+    const handleClick = (menu: string) => {
+        setSelectedMenu(menu);
+    };
     return (
         <div>
             <header>
                 <div className={styles.header_logo}>Constelink</div>
-                <div></div>
+
+
                 <ul className={styles.header_menu} >
-                    <li>공지사항</li>
-                    <li>기부하기</li>
-                    <li>치료일기</li>
-                    <li>치료달성</li>
+                    <li style={{
+                        color: selectedMenu === '공지사항' ? '#A19FD5' : '#A19FD5',
+                        borderBottom: selectedMenu === '공지사항' ? '3px solid #A19FD5' : '3px solid #A19FD5'
+                    }}
+                        onClick={() => handleClick('공지사항')} >공지사항</li>
+                    <li style={{
+                        color: selectedMenu === '기부하기' ? '#A19FD5' : 'black',
+                        borderBottom: selectedMenu === '기부하기' ? '3px solid #A19FD5' :  'none'
+                    }}
+                        onClick={() => handleClick('기부하기')}>기부하기</li>
+                    <li style={{
+                        color: selectedMenu === '치료일기' ? '#A19FD5' : 'black',
+                        borderBottom: selectedMenu === '치료일기' ? '3px solid #A19FD5' :  'none'
+                    }}
+                        onClick={() => handleClick('치료일기')}>치료일기</li>
+                    <li style={{
+                        color: selectedMenu === '치료달성' ? '#A19FD5' : 'black',
+                        borderBottom: selectedMenu === '치료달성' ? '3px solid #A19FD5' :  'none'
+                    }}
+                        onClick={() => handleClick('치료달성')}>치료달성</li>
                 </ul>
-               
-                <div></div>
-                <div className={styles.header_menu}>로그인</div>
+
+
+                <div className={styles.header_login}>로그인</div>
             </header>
 
         </div>
