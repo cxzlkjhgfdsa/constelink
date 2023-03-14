8e67bd471fc8514caaea03b049d4851417579ad3
gs20wow <gs20wow@gmail.com>
2023-03-14 17:35:18 +0900

    :lipstick: [FE] 치료일지 검색창 추가

--- Frontend/src/pages/RecoveryDiary.tsx
+++ Frontend/src/pages/RecoveryDiary.tsx
@@ -1,33 +1,25 @@
 import React from 'react';
+import searchicon from  "../assets/logo/search_icon.png";
 import styles from './RecoveryDiary.module.css';
 // import Header from 'components/header/Header';
+// header 코드 따로 빼서 넣어줘야 함
 
 const RecoveryDiary : React.FC = ()=>{
   return (
     <div>
-      {/* navbar 코드 */}
       <div className={styles.container}>
         <div className={styles.item}>
-          <h2>List 1</h2>
-          <ul>
-            <li>Item 1</li>
-          </ul>
-        </div>
-        <div className={styles.item}>
-          <h2>List 2</h2>
-          <ul>
-            <li>Item 4</li>
-            <li>Item 5</li>
-            <li>Item 6</li>
-          </ul>
-        </div>
-        <div className={styles.item}>
-          <h2>List 3</h2>
-          <ul>
-            <li>Item 7</li>
-            <li>Item 8</li>
-            <li>Item 9</li>
-          </ul>
+          <div className={styles.searchContainer}> 
+            <h2>치료일지</h2>
+            <div className={styles.searchInput}>
+            <input type="text" placeholder="병원 혹은 병명을 검색하세요" className={styles.searchInput} />
+            </div>
+            <img src={searchicon} alt="search" className={styles.searchIcon} />
+          </div>
+          <div>
+            <hr/>
+          </div>
+          <h1>내용들</h1>
         </div>
       </div>
     </div>
