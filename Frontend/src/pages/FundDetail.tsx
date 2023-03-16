a08d6994eddff7689cd992ea92a0ada0c2af2dc0
juggorr <juggorr@gmail.com>
2023-03-16 17:59:27 +0900

    :lipstick: [FE] 기부 상세페이지 UI 작업중

--- Frontend/src/pages/FundDetail.tsx
+++ Frontend/src/pages/FundDetail.tsx
@@ -2,7 +2,14 @@ import styles from "./FundDetail.module.css"
 
 const FundDetail: React.FC = () => {
 
+  const usageList: string[] = [
+    "허리 통증 마취비용",
+    "허리 통증 약제비용",
+    "이국종의 프리미엄 허리수술",
+  ]
+
   return (
+    <>
     <div className={styles.mainWrapper}>
       <div className={styles.fundMain}>
         <div className={styles.fundAbstract}>
@@ -21,11 +28,64 @@ const FundDetail: React.FC = () => {
             </div>
           </div>
           <div className={styles.fundShare}>
-            기부 공유
+            <div className={styles.shareTitle}>
+              모금알리기
+            </div>
+            <div className={styles.shareSub}>
+              모두에게 나눔을 공유하세요
+            </div>
+            <div className={styles.shareBtn}>
+              <span className={styles.shareBtnText}>
+                모금알리기
+              </span>
+            </div>
           </div>
         </div>
       </div>
     </div>
+
+    <div className={styles.storyWrapper}>
+      <div className={styles.storyDetail}>
+
+      </div>
+      <div className={styles.fundingCard}>
+        <div className={styles.fundingBeneficiary}>
+          <div className={styles.benefitTitle}>허리가 아픈 원철이는 치료비가 절실합니다.</div>
+          <div className={styles.benefitMan}>
+            <div className={styles.benefitTag}>
+              수혜자:
+            </div>
+            <div className={styles.benefitName}>
+              정원철
+            </div>
+            <div className={styles.benefitImg} />
+          </div>
+        </div>
+        <div className={styles.fundingUsage}>
+          <div className={styles.usageTag}>
+            치료비는 다음과 같은 곳에 사용됩니다
+          </div>
+          {
+            usageList.map(usage => {
+              return <li className={styles.usageList}>{usage}</li>
+            })
+          }
+          <div className={styles.usageHospital}>
+            서울아산병원
+          </div>
+        </div>
+
+        <div className={styles.fundingBtn}>
+          <div className={styles.fundingBtnText}>
+            모금동참
+          </div>
+        </div>
+        <div className={styles.fundingBar}>
+
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
