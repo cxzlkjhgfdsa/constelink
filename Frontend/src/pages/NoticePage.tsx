fc3880c3849f9e12499a62976601e19538604712
gksrud316 <gksrud316@naver.com>
2023-03-17 17:48:09 +0900

    :sparkles: [FE] 공지사항 작성페이지 작업
    - 공지사항 작성 페이지 UI 설정
    - 작성 데이터 Set 설정
    - sun editor 설정

--- Frontend/src/pages/NoticePage.tsx
+++ Frontend/src/pages/NoticePage.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import styles from './NoticePage.module.css';
 import Pagination from "react-js-pagination";
 import { useState } from 'react';
+import axios from 'axios';
+
+import SunEditor from 'suneditor-react';
+import 'suneditor/dist/css/suneditor.min.css';
 const NoticePage: React.FC = () => {
 
+    const selectRef = useRef<HTMLSelectElement>(null);
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [fixedType, setFixedType] = useState("off");
+    // const [content, setContent] = useState("");
 
 
+
+    const [content, setContent] = useState('');
+
+    const handleEditorChange = (value: string) => {
+        setContent(value);
+    };
+    const handleClick = () => {
+        const selectedValue = selectRef.current?.value;
+        const inputValue = inputRef.current?.value;
+        const fixed = fixedType;
+        const contents = content;
+
+        axios.post("")
+    };
+    const contentChangeHandler = (e: string) => {
+        console.log(e);
+
+    }
 
     return (
         <div className={styles.NoticePage}>
@@ -32,35 +58,64 @@ const NoticePage: React.FC = () => {
             <section className={styles.write_section} >
 
                 <div className={styles.write_title}>
-                    <input type="text" className={styles.write_input} placeholder={"제목"}/>
+                    <input type="text" className={styles.write_input} placeholder={"제목"} ref={inputRef} />
                 </div>
-                
+
                 <div className={styles.write_category}>
                     <div className={styles.category_select}>
                         <div>카테고리 분류</div>
-                        <select name="" id="">
-                            <option value="일반공지">일반 공지</option>
+                        <label htmlFor="types" />
+                        <select ref={selectRef} name="types">
+                            <option value="1">일반 공지</option>
+                            <option value="2">긴급 공지</option>
                         </select>
-
-
                     </div>
+
                     <div className={styles.category_fixed} >
                         <div>상단고정</div>
-                        <button>ON</button>
-
+                        <input type="button" value={fixedType} onClick={() => fixedType === "off" ? setFixedType("on") : setFixedType("off")} />
                     </div>
+
                 </div>
+
 
                 <div className={styles.write_category}>
                     <div>글작성</div>
                 </div>
 
-
+                <div className={styles.write_content}>
+                    <SunEditor
+                        lang="en"
+                        width="920px"
+                        height="400px"
+                        autoFocus={false}
+                        onChange={contentChangeHandler}
+                        setDefaultStyle="font-family:Hahmlet;color:darkgrey;font-size: 20px;"
+                        placeholder="환자의 치료일지를 적어주세요"
+                        setOptions={{
+                            buttonList: [
+                                [
+                                    "bold",
+                                    "underline",
+                                    "table",
+                                    "image",
+                                    "video",
+                                    "audio",
+                                    "italic",
+                                    "fontSize",
+                                    "formatBlock",
+                                    "list",
+                                    "fontColor"
+                                ]
+                            ]
+                        }}
+                    />
+                </div>
             </section>
 
 
-
-
+            <button onClick={handleClick}>작성완료</button>
+            {/* <a href='http://j8a206.p.ssafy.io:8997/oauth2/authorization/kakao'>ddd</a> */}
         </div>
     );
 };
