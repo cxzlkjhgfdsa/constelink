53e92d5e98ffa49123e4600df1bab882d9cba4f5
gs20wow <gs20wow@gmail.com>
2023-03-21 17:52:36 +0900

    :white_check_mark: [FE] App.js 변경

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 import React, { useState } from 'react';
 import './App.css';
-import MainLayout  from "./components/MainLayout";
 import Header from './components/header/Header';
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
@@ -11,20 +10,18 @@ import RecoveryDiaryDetail from './pages/RecoveryDiaryDetail';
 
 
 function App() {
-
   return (
     <BrowserRouter>
         <div className="App">
+        <Header />
           <Routes>
               {/* <Login/> */}
-              <Route element={<MainLayout />}>
-                {/* <Route path='/fundmain' element={<FundMain />} />
-                <Route path='/funddetail' element={<FundDetail />} />
-                <Route path='/fundpayment' element={<FundPayment />} /> */}
-                <Route path='/' element={<HomePage/>}/>
-                <Route path='/diary' element={<RecoveryDiary/>}/>
-                <Route path='/diarydetail/??' element={<RecoveryDiaryDetail/>}/>
-              </Route>
+              {/* <Route path='/fundmain' element={<FundMain />} />
+              <Route path='/funddetail' element={<FundDetail />} />
+              <Route path='/fundpayment' element={<FundPayment />} /> */}
+              <Route path='/' element={<HomePage/>}/>
+              <Route path='/diary' element={<RecoveryDiary/>}/>
+              <Route path='/diarydetail/:index' element={<RecoveryDiaryDetail/>}/>
           </Routes>
         </div>
     </BrowserRouter>
