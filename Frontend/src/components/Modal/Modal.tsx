ac8312d740ae3b09e3459f07077c1ecc9f9043d4
gs20wow <gs20wow@gmail.com>
2023-03-21 17:53:07 +0900

    :lipstick: [FE] 모달페이지 생성

--- Frontend/src/components/Modal/Modal.tsx
+++ Frontend/src/components/Modal/Modal.tsx
@@ -1,34 +1,31 @@
-import React from 'react';
-import styles from "./Modal.module.css";
+import React, {PropsWithChildren} from "react";
+import styles from './Modal.module.css';
 
-interface ModalProps {
-  open: boolean;
-  close: () => void;
-  header: string;
-  children: React.ReactNode; // Define the type of children explicitly
+interface ModalDefaultType {
+  onClickToggleModal: () => void;
 }
 
-const Header: React.FC<ModalProps> = ({ open, close, header, children }) => {
+// 모달 기본 형식
+function Modal({
+  onClickToggleModal,
+  children,
+}: PropsWithChildren<ModalDefaultType>) {
   return (
-    <div className={open ? `${styles.openModal} ${styles.modal}` : styles.modal}>
-      {open ? (
-        <section>
-          <header>
-            {header}
-            <button className="close" onClick={close}>
-              &times;
-            </button>
-          </header>
-          <main>{children}</main>
-          <footer>
-            <button className="close" onClick={close}>
-              close
-            </button>
-          </footer>
-        </section>
-      ) : null}
+    <div className={styles.ModalContainer}>
+      <div className={styles.DialogBox}>
+        {children}
+        </div>
+        <div className={styles.Backdrop}
+          onClick={(e: React.MouseEvent) => {
+            e.preventDefault();
+            if (onClickToggleModal) {
+              onClickToggleModal();
+            }
+          }}
+          >
+        </div>
     </div>
   );
-};
+}
 
-export default Header;
+export default Modal;
