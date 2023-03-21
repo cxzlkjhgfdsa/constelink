4df0aea00c8976289b14d36151d069e42e07049e
gs20wow <gs20wow@gmail.com>
2023-03-21 17:53:43 +0900

    :lipstick: [FE] 모달페이지로 인한 치료일지 스타일 및 컴포넌트 등록

--- Frontend/src/pages/RecoveryDiary.tsx
+++ Frontend/src/pages/RecoveryDiary.tsx
@@ -10,36 +10,42 @@ import profileImg3 from "../assets/img/profile3.png";
 // 카드 배열
 const cards = [
   {
+    name: "권철순",
     title: "치료일지 1",
     image: profileImg1,
     content: ["췌장암", "허리골절", "습진","췌장암", "허리골절", "습진","췌장암", "허리골절", "습진"],
     hospital: "서울대병원"
   },
   {
+    name: "이운재",
     title: "치료일지 2",
     image: profileImg2,
     content: ["대장암"],
     hospital: "길병원"
   },
   {
+    name: "김병지",
     title: "치료일지 4",
     image: profileImg3,
     content: ["골절"],
     hospital: "을지대병원"
   },
   {
+    name: "박지성",
     title: "치료일지 5",
     image: profileImg1,
     content: ["골절"],
     hospital: "세브란스병원"
   },
   {
+    name: "손흥민",
     title: "치료일지 6",
     image: profileImg2,
     content: ["위궤양"],
     hospital: "길병원"
   },
   {
+    name: "비가오는날에",
     title: "치료일지 7",
     image: profileImg3,
     content: ["간암"],
@@ -54,10 +60,11 @@ const RecoveryDiary : React.FC = ()=>{
   // Card title에 검색어가 있는지 여부를 확인
   const filteredCards = searchKeyword ? 
       cards.filter((card) => {
+        const isInName = card.name.includes(searchKeyword);
         const isInTitle = card.title.includes(searchKeyword);
         const isInContent = card.content.some((item) => item.includes(searchKeyword));
         const isInHospital = card.hospital.includes(searchKeyword);
-        return isInTitle || isInContent || isInHospital;
+        return isInTitle || isInContent || isInHospital || isInName; 
       }) :
       cards;
     // cards.filter((card) => card.title.includes(searchKeyword)) :
@@ -69,7 +76,7 @@ const RecoveryDiary : React.FC = ()=>{
     
   // 클릭 시 디테일페이지로 이동하는 함수
     const handleCardClick = (id: number) => {
-      navigate(`/diarydetail/${id}`);
+      navigate(`/diarydetail/${id+1}`);
     }
 
 
@@ -106,29 +113,33 @@ const RecoveryDiary : React.FC = ()=>{
 
           {/* 검색어에 입력한대로 반응형 결과 도출 */}
           {filteredCards.map((card, index) => (
-            <div className={styles.card} key={index}>
-              <div className={styles.cardTop}>
+          <div className={styles.card} key={index}>
+            <div className={styles.cardTop}></div>
+            <div className={styles.imageContainer}>
+              <img src={card.image} alt="profile" className={styles.image} />
+            </div>
+            <div className={styles.titleContent}>
+            <p className={styles.name}>
+            {card.name.length > 3 
+             ? card.name.substring(0, 1) + "X" + card.name.substring(2,3)   
+             : card.name.substring(0, 1) + "X" + card.name.substring(2)}
+            님
+            </p>
+              <div className={styles.content}>
+                {card.content.map((item, index) => (
+                  <div className={styles.contentItem} key={index}>
+                    {item}
+                  </div>
+                ))}
               </div>
-              <div className={styles.imageContainer}>
-                <img src={card.image} alt="profile" className={styles.image} />
-              </div>
-              <div className={styles.titleContent}>
-                <p className={styles.title}>{card.title}</p>
-                <div className={styles.content}>
-                  {card.content.map((item, index) => (
-                    <div className={styles.contentItem} key={index}>
-                      {item}
-                    </div>
-                  ))}
-                </div>
-              </div>
-              {/* 더 알아보기 버튼(디테일페이지로 이동) */}
-              <div className={styles.bottomContent}>
-                <div className={styles.detailButton}
-                onClick={()=> handleCardClick(index+1)}
-                >더 알아보기</div>
-              </div>
-              </div>
+            </div>
+            {/* 더 알아보기 버튼(디테일페이지로 이동) */}
+            <div className={styles.bottomContent}>
+              <div className={styles.detailButton}
+              onClick={()=> handleCardClick(index)}
+              >더 알아보기</div> 
+            </div>
+          </div>
           ))}
         </div>
       </div>
--- Frontend/src/pages/RecoveryDiaryDetail.tsx
+++ Frontend/src/pages/RecoveryDiaryDetail.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState, useCallback }  from 'react';
 import styles from './RecoveryDiaryDetail.module.css';
 import profileImg1 from "../assets/img/profile1.png";
 import cardImg1 from "../assets/img/cardImg1.png";
 import { useParams } from 'react-router-dom';
-
+import Modal from "../components/Modal/Modal";
 
 const RecoveryDiaryDetail: React.FC = () => {
-  const { index } = useParams<{ index: string }>();
+  // 기본 string 설정
+  // const { index } = useParams<{ index: string }>();
+  // parseInt 함수의 두번째 인자로 10을 전달하여 10진수로 변환하도록 설정
+  // const indexAsNumber = parseInt(index || "0", 10);
+
+  // 모달이 열려있는지 여부 확인
+  const [isOpenModal, setOpenModal] = useState<boolean>(false);
+  const [selectedRecordIndex, setSelectedRecordIndex] = useState<number | null>(null);
+  
+  // 클릭했을 때 모달 열고 닫기
+  const onClickToggleModal = useCallback(() => {
+    setOpenModal(!isOpenModal);
+  }, [isOpenModal]); 
   // Use `id` to get the cardIndex data from the backend
   
+  // 레코드를 클릭할 때마다 선택한 인덱스 저장
+  const onClickRecord = useCallback((index: number) => {
+    setSelectedRecordIndex(index);
+    setOpenModal(true);
+  }, []);
+
   const cardIndex = {
     title: "치료일지",
     image: profileImg1,
-    content: ["허리골절"],
+    index: ["허리골절"],
     hospital: "서울대병원",
     patient: {
       name: "홍길동",
@@ -27,33 +45,51 @@ const RecoveryDiaryDetail: React.FC = () => {
     {
     date: "2022-01-01",
     image: cardImg1,
-    content: "첫 번째 치료 내용"
+    index: "첫 번째 치료일지",
+    content : "오늘은 드디어 다시 일어날 수 있게 되었습니다. 후원 감사합니다."
     },
     {
     date: "2022-01-02",
     image: cardImg1,
-    content: "두 번째 치료 내용"
+    index: "두 번째 치료일지",
+    content : "오늘은 드디어 다시 일어날 수 있게 되었습니다. 후원 감사링."
     },
     {
     date: "2022-01-03",
     image: cardImg1,
-    content: "세 번째 치료 내용"
+    index: "세 번째 치료일지",
+    content : "오늘은 드디어 다시 일어날 수 있게 되었습니다. 후원 감사쓰리."
     },
     {
     date: "2022-01-04",
     image: cardImg1,
-    content: "네 번째 치료 내용"
+    index: "네 번째 치료일지",
+    content : "오늘은 드디어 다시 일어날 수 있게 되었습니다. 후원 감사포."
     },
     {
     date: "2022-01-05",
     image: cardImg1,
-    content: "다섯 번째 치료 내용"
+    index: "다섯 번째 치료일지",
+    content : "오늘은 드디어 다시 일어날 수 있게 되었습니다. 후원 감사합니다."
     },
     {
-      date: "2022-01-09",
-      image: cardImg1,
-      content: "여섯 번째 치료 내용"
-      },
+    date: "2022-01-09",
+    image: cardImg1,
+    index: "여섯 번째 치료일지",
+    content : "오늘은 드디어 다시 일어날 수 있게 되었습니다. 후원 감사합니다."
+    },
+    {
+    date: "2022-01-09",
+    image: cardImg1,
+    index: "일곱 번째 치료일지",
+    content : "오늘은 드디어 다시 일어날 수 있게 되었습니다. 후원 감사합니다."
+    },
+    {
+    date: "2022-01-09",
+    image: cardImg1,
+    index: "여덟 번째 치료일지",
+    content : "오늘은 드디어 다시 일어날 수 있게 되었습니다. 후원 감사합니다."
+    },
     ];
 
   return (
@@ -85,23 +121,35 @@ const RecoveryDiaryDetail: React.FC = () => {
                   <p className={styles.patientInfoContent}>{cardIndex.patient.hospital}</p>
                 </div>
                 <div className={styles.patientInfoItem}>
-                  <p className={styles.patientInfoTitle}>모금액</p>
+                  <p className={styles.patientInfoTitle}>총 모금액</p>
                   <p className={styles.patientInfoContent}>{cardIndex.patient.amountRaised}</p>
                 </div>
             </div>
           </div>
         </div>
-
-        <div className={styles.treatmentRecords}>
+        <hr className={styles.hr}/>
         {treatmentRecords.map((record, index) => (
-          <div key={index} className={styles.record}>
-            <div className={styles.recordDate}>{record.date}</div>
-            <img className={styles.recordImage} src={record.image} alt="treatment" />
-            <div className={styles.recordContent}>{record.content}</div>
+        <div key={index} className={styles.record} onClick={() => onClickRecord(index)}>
+          <div className={styles.recordDate}>{record.date}</div>
+          <img className={styles.recordImage} src={record.image} alt="treatment" />
+          <div className={styles.recordIndex}>{record.index}</div>
+          <div className={styles.recordContent}>
+          {record.content.length > 10 ? `${record.content.substring(0, 10)}...` : record.content}
           </div>
-          ))}
         </div>
+        ))}
       </div>
+      {isOpenModal && selectedRecordIndex !== null && (
+        <Modal onClickToggleModal={onClickToggleModal}>
+            <img src={treatmentRecords[selectedRecordIndex].image} alt="treatment" className={styles.modalImage} />
+            <div className={styles.modalInfo}>
+              <div className={styles.modalInfoTitle}>{treatmentRecords[selectedRecordIndex].index}</div>
+              <hr className={styles.modalHr}/>
+              <div className={styles.modalInfoDate}>작성일 : {treatmentRecords[selectedRecordIndex].date}</div>
+              <div className={styles.modalInfoContent}>{treatmentRecords[selectedRecordIndex].content}</div>
+            </div>
+        </Modal>
+      )}
     </div>
   );
 };
