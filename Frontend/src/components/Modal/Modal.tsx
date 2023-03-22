e009fc390e528da5079aba0517c4c47f4409925c
gs20wow <gs20wow@gmail.com>
2023-03-22 17:46:40 +0900

    :lipstick: [FE] 치료일지 생성, 취소

--- Frontend/src/components/Modal/Modal.tsx
+++ Frontend/src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, {PropsWithChildren} from "react";
+import React, {PropsWithChildren, useEffect} from "react";
 import styles from './Modal.module.css';
 
 interface ModalDefaultType {
@@ -6,10 +6,23 @@ interface ModalDefaultType {
 }
 
 // 모달 기본 형식
-function Modal({
-  onClickToggleModal,
-  children,
-}: PropsWithChildren<ModalDefaultType>) {
+function Modal({ onClickToggleModal, children}: PropsWithChildren<ModalDefaultType>) {
+  // 모달이 떠있으면 스크롤 방지하는 함수 
+  useEffect(() => {
+    function disableScroll() {
+      document.body.style.overflow = "hidden";
+      document.body.style.paddingRight = "22px";
+    }
+    function enableScroll() {
+      document.body.style.overflow = "";
+      document.body.style.paddingRight = "";
+    }
+    // modal이 떠 있을 땐 스크롤 막음
+    disableScroll();
+    // modal 닫히면 다시 스크롤 가능하도록 함
+    return () => enableScroll();
+  }, []);
+
   return (
     <div className={styles.ModalContainer}>
       <div className={styles.DialogBox}>
--- Frontend/src/pages/RecoveryDiaryDetail.tsx
+++ Frontend/src/pages/RecoveryDiaryDetail.tsx
@@ -2,20 +2,41 @@ import React, { useState, useCallback }  from 'react';
 import styles from './RecoveryDiaryDetail.module.css';
 import profileImg1 from "../assets/img/profile1.png";
 import cardImg1 from "../assets/img/cardImg1.png";
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import Modal from "../components/Modal/Modal";
 
-const RecoveryDiaryDetail: React.FC = () => {
-  // 기본 string 설정
-  // const { index } = useParams<{ index: string }>();
-  // parseInt 함수의 두번째 인자로 10을 전달하여 10진수로 변환하도록 설정
-  // const indexAsNumber = parseInt(index || "0", 10);
+interface TreatmentRecord {
+  index: number;
+  image: string;
+  date: string;
+  content: string;
+}
 
+const RecoveryDiaryDetail: React.FC = () => {
+  // 병원계정일 때 설정 필요
+  const [isChecked, setChecked] = useState<boolean>(false);
+  
+  // 이동
+  const navigate = useNavigate();
+
+  // 치료일지 생성 
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const [date, setDate] = useState('');
+  const [content, setContent] = useState('');
+
+  // 기본 string 설정
+  const { index } = useParams<{ index: string }>();
+  // parseInt 함수의 두번째 인자로 10을 전달하여 10진수로 변환하도록 설정
+  const indexAsNumber = parseInt(index || "0", 10);
+  
   // 모달이 열려있는지 여부 확인
   const [isOpenModal, setOpenModal] = useState<boolean>(false);
   const [selectedRecordIndex, setSelectedRecordIndex] = useState<number | null>(null);
   
-  // 클릭했을 때 모달 열고 닫기
+  // axios
+  const [treatmentRecords, setTreatmentRecords] = useState<TreatmentRecord[]>([]);
+
+  // 생성되어있는 카드 클릭했을 때 모달 열고 닫기
   const onClickToggleModal = useCallback(() => {
     setOpenModal(!isOpenModal);
   }, [isOpenModal]); 
@@ -27,6 +48,98 @@ const RecoveryDiaryDetail: React.FC = () => {
     setOpenModal(true);
   }, []);
 
+  // 일지 생성 버튼 클릭 시 모달 띄우기
+  const [recordData, setRecordData] = useState<TreatmentRecord>({
+    index: treatmentRecords.length + 1, // index는 이전 데이터의 index + 1로 설정
+    image: '',
+    date: '',
+    content: '',
+  });
+  
+  // 모달 열기 
+  const onClickCreateRecord = useCallback(() => {
+    setOpenModal(true);
+  }, []);
+  
+  // 생성완료 버튼 클릭 시 데이터 추가
+  const onAddRecord = useCallback(() => {
+    if (!imageFile || !date || !content) {
+      alert('모든 항목을 입력해주세요.');
+      return;
+    }
+    
+    const newRecord: TreatmentRecord = {
+      index: treatmentRecords.length + 1,
+      image: URL.createObjectURL(imageFile),
+      date,
+      content,
+    };
+
+    setTreatmentRecords((prev) => [...prev, newRecord]);
+    setImageFile(null);
+    setDate('');
+    setContent('');
+    setOpenModal(false);
+  }, [imageFile, date, content, treatmentRecords]);
+
+// 수정완료 버튼 클릭 시 데이터 수정
+const onEditRecord = useCallback(() => {
+  if (!selectedRecordIndex) {
+    alert('수정할 항목을 선택해주세요.');
+    return;
+  }
+
+  if (!imageFile || !date || !content) {
+    alert('모든 항목을 입력해주세요.');
+    return;
+  }
+
+  const updatedRecord: TreatmentRecord = {
+    ...treatmentRecords[selectedRecordIndex],
+    image: URL.createObjectURL(imageFile),
+    date,
+    content,
+  };
+
+    setTreatmentRecords((prev) => {
+      const newTreatmentRecords = [...prev];
+      newTreatmentRecords[selectedRecordIndex] = updatedRecord;
+      return newTreatmentRecords;
+    });
+    setImageFile(null);
+    setDate('');
+    setContent('');
+    setOpenModal(false);
+    }, [selectedRecordIndex, imageFile, date, content, treatmentRecords]);
+
+  // 취소버튼
+  const onCancelRecord = useCallback(() => {
+    setImageFile(null);
+    setDate('');
+    setContent('');
+    setOpenModal(false);
+  }, []);
+
+  // 데이터 삭제 메서드
+  const onRemoveRecord = useCallback(() => {
+    const confirmResult = window.confirm('정말로 삭제하시겠습니까?');
+    if (confirmResult && selectedRecordIndex !== null) {
+      setTreatmentRecords((prev) => {
+        const newTreatmentRecords = [...prev];
+        newTreatmentRecords.splice(selectedRecordIndex, 1);
+        return newTreatmentRecords;
+      });
+      setSelectedRecordIndex(null);
+      setOpenModal(false);
+    }
+  }, [selectedRecordIndex, treatmentRecords]);
+
+  // 목록으로 이동하는 버튼
+  const handleBackButtonClick = () => {
+    navigate(`/diary`);
+  }
+
+  // 임의로 만든 데이터
   const cardIndex = {
     title: "치료일지",
     image: profileImg1,
@@ -41,60 +154,10 @@ const RecoveryDiaryDetail: React.FC = () => {
     }, 
   };
 
-  const treatmentRecords = [
-    {
-    date: "2022-01-01",
-    image: cardImg1,
-    index: "첫 번째 치료일지",
-    content : "오늘은 드디어 다시 일어날 수 있게 되었습니다. 후원 감사합니다."
-    },
-    {
-    date: "2022-01-02",
-    image: cardImg1,
-    index: "두 번째 치료일지",
-    content : "오늘은 드디어 다시 일어날 수 있게 되었습니다. 후원 감사링."
-    },
-    {
-    date: "2022-01-03",
-    image: cardImg1,
-    index: "세 번째 치료일지",
-    content : "오늘은 드디어 다시 일어날 수 있게 되었습니다. 후원 감사쓰리."
-    },
-    {
-    date: "2022-01-04",
-    image: cardImg1,
-    index: "네 번째 치료일지",
-    content : "오늘은 드디어 다시 일어날 수 있게 되었습니다. 후원 감사포."
-    },
-    {
-    date: "2022-01-05",
-    image: cardImg1,
-    index: "다섯 번째 치료일지",
-    content : "오늘은 드디어 다시 일어날 수 있게 되었습니다. 후원 감사합니다."
-    },
-    {
-    date: "2022-01-09",
-    image: cardImg1,
-    index: "여섯 번째 치료일지",
-    content : "오늘은 드디어 다시 일어날 수 있게 되었습니다. 후원 감사합니다."
-    },
-    {
-    date: "2022-01-09",
-    image: cardImg1,
-    index: "일곱 번째 치료일지",
-    content : "오늘은 드디어 다시 일어날 수 있게 되었습니다. 후원 감사합니다."
-    },
-    {
-    date: "2022-01-09",
-    image: cardImg1,
-    index: "여덟 번째 치료일지",
-    content : "오늘은 드디어 다시 일어날 수 있게 되었습니다. 후원 감사합니다."
-    },
-    ];
-
   return (
     <div className={styles.container}>
       <div className={styles.item}>
+        {/* 치료일기에서 넘겨받은 데이터를 토대로, 확인 가능한 세부정보 */}
         <div className={styles.cardIndex}>
           <div className={styles.cardTop}>
             <p className={styles.title}>{cardIndex.title}</p>
@@ -128,6 +191,7 @@ const RecoveryDiaryDetail: React.FC = () => {
           </div>
         </div>
         <hr className={styles.hr}/>
+        {/* 치료일지 카드들 */}
         {treatmentRecords.map((record, index) => (
         <div key={index} className={styles.record} onClick={() => onClickRecord(index)}>
           <div className={styles.recordDate}>{record.date}</div>
@@ -138,19 +202,95 @@ const RecoveryDiaryDetail: React.FC = () => {
           </div>
         </div>
         ))}
+        <div className={styles.detailButton}>
+            <button className={styles.detailCreateButton} onClick={() => onClickCreateRecord()}>치료일지 생성</button>
+            <button className={styles.detailBackButton} onClick={()=>handleBackButtonClick()}>목록보기</button>
+        </div>
       </div>
+
+      {/* 카드를 눌렀을 때 모달 발생 */}
+      {/* 기부자가 클릭시에 확인 / 병원 계정으로 클릭 시에 수정, 삭제 isChecked*/}
       {isOpenModal && selectedRecordIndex !== null && (
-        <Modal onClickToggleModal={onClickToggleModal}>
-            <img src={treatmentRecords[selectedRecordIndex].image} alt="treatment" className={styles.modalImage} />
-            <div className={styles.modalInfo}>
-              <div className={styles.modalInfoTitle}>{treatmentRecords[selectedRecordIndex].index}</div>
-              <hr className={styles.modalHr}/>
-              <div className={styles.modalInfoDate}>작성일 : {treatmentRecords[selectedRecordIndex].date}</div>
-              <div className={styles.modalInfoContent}>{treatmentRecords[selectedRecordIndex].content}</div>
+      <Modal onClickToggleModal={onClickToggleModal}>
+        <div className={styles.modalTop}>
+        </div>
+        <img src={treatmentRecords[selectedRecordIndex].image} alt="treatment" className={styles.modalImage} />
+        <div className={styles.modalInfo}>
+          <div className={styles.modalInfoTitle}>{treatmentRecords[selectedRecordIndex].index}</div>
+          <hr className={styles.modalHr} />
+          <div className={styles.modalInfoDate}>작성일 : {treatmentRecords[selectedRecordIndex].date}</div>
+          <div className={styles.modalInfoContent}>{treatmentRecords[selectedRecordIndex].content}</div>
+          {!isChecked && (
+            <div className={styles.modalButton}>
+              <button className={styles.modalButtonItem} onClick={() => onEditRecord()}>수정</button>
+              <button className={styles.modalButtonItem} onClick={() => onCancelRecord()}>취소</button>
+              <button className={styles.modalButtonItem} onClick={() => onRemoveRecord()}>삭제</button>
             </div>
-        </Modal>
+          )}
+          {isChecked && (
+            <div className={styles.modalButton}>
+              <button className={styles.modalButtonItem}>확인</button>
+            </div>
+          )}
+            </div>
+      </Modal>
       )}
-    </div>
+      
+      {/* 병원계정 -> 치료일지 생성 */}
+      {isOpenModal && (
+            <Modal onClickToggleModal={onClickToggleModal}>
+              <div className={styles.modalTop}>
+              <h2 className={styles.modalTitle}>병원 치료일지 생성</h2>
+              </div>
+            <form className={styles.modalForm}>
+              <div className={styles.modalFormRow}>
+                <label htmlFor="imageFile" className={styles.modalFormLabel}>
+                이미지 파일
+                </label>
+                <input
+                id="imageFile"
+                type="file"
+                accept="image/*"
+                onChange={(e) => setImageFile(e.target.files?.[0] || null)}
+                className={styles.modalFormInput}
+                />
+              </div>
+              <hr/>
+              <div className={styles.modalFormRow}>
+                <label htmlFor="date" className={styles.modalFormLabel}>
+                  날짜
+                </label>
+                <input
+                  id="date"
+                  type="date"
+                  value={date}
+                  onChange={(e) => setDate(e.target.value)}
+                  className={styles.modalFormInput}
+                />
+              </div>
+              <div className={styles.modalFormRow}>
+                <label htmlFor="content" className={styles.modalFormLabel}>
+                  내용
+                </label>
+                <textarea
+                  id="content"
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
+                  className={styles.modalFormTextarea}
+                ></textarea>
+              </div>
+              </form>
+              <div className={styles.modalButton}>
+                <button className={styles.modalButtonItem} onClick={onAddRecord}>
+                  생성완료
+                </button>
+                <button className={styles.modalButtonItem} onClick={onCancelRecord}>
+                  취소
+                </button>
+              </div>
+            </Modal>
+            )}  
+      </div>
   );
 };
 
