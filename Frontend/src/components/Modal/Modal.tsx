dcc2f56939d5a2f0a925cb36132fbc1d324625f1
gs20wow <gs20wow@gmail.com>
2023-03-23 17:47:51 +0900

    :lipstick: [FE] 치료일지 axios 연결 중간구현

--- Frontend/src/components/Modal/Modal.tsx
+++ Frontend/src/components/Modal/Modal.tsx
@@ -6,7 +6,7 @@ interface ModalDefaultType {
 }
 
 // 모달 기본 형식
-function Modal({ onClickToggleModal, children}: PropsWithChildren<ModalDefaultType>) {
+function Modal({onClickToggleModal, children}: PropsWithChildren<ModalDefaultType>) {
   // 모달이 떠있으면 스크롤 방지하는 함수 
   useEffect(() => {
     function disableScroll() {
--- Frontend/src/pages/RecoveryDiaryDetail.tsx
+++ Frontend/src/pages/RecoveryDiaryDetail.tsx
@@ -1,9 +1,13 @@
-import React, { useState, useCallback }  from 'react';
+import React, { useState, useCallback, useRef }  from 'react';
 import styles from './RecoveryDiaryDetail.module.css';
 import profileImg1 from "../assets/img/profile1.png";
 import cardImg1 from "../assets/img/cardImg1.png";
 import { useParams, useNavigate } from 'react-router-dom';
 import Modal from "../components/Modal/Modal";
+import SunEditor from 'suneditor-react';
+import SunEditorCore from "suneditor/src/lib/core";
+import 'suneditor/dist/css/suneditor.min.css';
+import axios from 'axios';
 
 interface TreatmentRecord {
   index: number;
@@ -13,8 +17,18 @@ interface TreatmentRecord {
 }
 
 const RecoveryDiaryDetail: React.FC = () => {
-  // 병원계정일 때 설정 필요
-  const [isChecked, setChecked] = useState<boolean>(false);
+
+  const selectRef = useRef<HTMLSelectElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [fixedType, setFixedType] = useState(false);
+
+  // 기본 string 설정
+  const { index } = useParams<{ index: string }>();
+  // parseInt 함수의 두번째 인자로 10을 전달하여 10진수로 변환하도록 설정
+  const indexAsNumber = parseInt(index || "0", 10);
+
+  // 병원계정일 때 -> 설정 필요
+  const [isChecked, setChecked] = useState<boolean>(true);
   
   // 이동
   const navigate = useNavigate();
@@ -22,77 +36,111 @@ const RecoveryDiaryDetail: React.FC = () => {
   // 치료일지 생성 
   const [imageFile, setImageFile] = useState<File | null>(null);
   const [date, setDate] = useState('');
+
+  // get 컨텐츠
   const [content, setContent] = useState('');
 
-  // 기본 string 설정
-  const { index } = useParams<{ index: string }>();
-  // parseInt 함수의 두번째 인자로 10을 전달하여 10진수로 변환하도록 설정
-  const indexAsNumber = parseInt(index || "0", 10);
+  // 수정된 컨텐츠
+  const [contents, setContents] = useState('');
   
   // 모달이 열려있는지 여부 확인
   const [isOpenModal, setOpenModal] = useState<boolean>(false);
-  const [selectedRecordIndex, setSelectedRecordIndex] = useState<number | null>(null);
-  
-  // axios
-  const [treatmentRecords, setTreatmentRecords] = useState<TreatmentRecord[]>([]);
-
-  // 생성되어있는 카드 클릭했을 때 모달 열고 닫기
+  // 카드 클릭했을 때 모달 열고 닫기
   const onClickToggleModal = useCallback(() => {
     setOpenModal(!isOpenModal);
   }, [isOpenModal]); 
   // Use `id` to get the cardIndex data from the backend
   
-  // 레코드를 클릭할 때마다 선택한 인덱스 저장
-  const onClickRecord = useCallback((index: number) => {
-    setSelectedRecordIndex(index);
-    setOpenModal(true);
-  }, []);
-
-  // 일지 생성 버튼 클릭 시 모달 띄우기
-  const [recordData, setRecordData] = useState<TreatmentRecord>({
-    index: treatmentRecords.length + 1, // index는 이전 데이터의 index + 1로 설정
-    image: '',
-    date: '',
-    content: '',
-  });
+  // axios
+  const [treatmentRecords, setTreatmentRecords] = useState<TreatmentRecord[]>([]);
   
-  // 모달 열기 
+  // 생성되어 있는 카드를 선택할 때 올바른 정보를 도출
+  const [selectedRecordIndex, setSelectedRecordIndex] = useState<number | null>(null);
+  
+  const contentChangeHandler = (e: string) => {
+    console.log(e);
+    setContents(e)
+}
+
+  const handleEditorChange = (value: string) => {
+    setContent(value);
+  };
+  const handleClick = () => {
+    // api 설정 예정
+    const selectedValue = selectRef.current?.value;
+    const inputValue = inputRef.current?.value;
+    const fixed = fixedType;
+    const contents = content;
+    console.log(content)
+  };
+
+
+const editor = useRef<SunEditorCore>();
+// The sunEditor parameter will be set to the core suneditor instance when this function is called
+const getSunEditorInstance = (sunEditor: SunEditorCore) => {
+    editor.current = sunEditor;
+    console.log(1)
+    console.log(editor.current);
+
+};
+
+  // 모달 관련 함수 시작
+  // 생성되어있는 카드를 클릭할 때 선택한 인덱스 저장
+  const onClickRecord = useCallback((index :number) => {
+    setOpenModal(true);
+    setChecked(false);
+    setSelectedRecordIndex(index);
+  }, []);
+  
+  
+  // 치료일지 생성 버튼
   const onClickCreateRecord = useCallback(() => {
     setOpenModal(true);
+    setChecked(true);
   }, []);
   
-  // 생성완료 버튼 클릭 시 데이터 추가
-  const onAddRecord = useCallback(() => {
+  // 모달 속 생성버튼
+  const onAddRecord = () => {
+    console.log(content)
+    console.log(contents)
+    
+  }
+
+  // const onAddRecord = 
+  // useCallback(() => {
+  //   if (!imageFile || !content) {
+  //     alert('내용을 입력해주세요.');
+  //     return;
+  //   }
+    
+  //   const newRecord: TreatmentRecord = {
+  //     index: treatmentRecords.length + 1,
+  //     image: URL.createObjectURL(imageFile),
+  //     date,
+  //     content,
+  //   };
+
+  //   setTreatmentRecords((prev) => [...prev, newRecord]);
+  //   setImageFile(null);
+  //   setDate('');
+  //   setContent('');
+  //   setOpenModal(false);
+  //   [imageFile, date, content, treatmentRecords]
+  // }, 
+  // );
+
+// 모달 속 수정버튼
+
+  const onEditRecord = useCallback(() => {
     if (!imageFile || !date || !content) {
       alert('모든 항목을 입력해주세요.');
       return;
     }
-    
-    const newRecord: TreatmentRecord = {
-      index: treatmentRecords.length + 1,
-      image: URL.createObjectURL(imageFile),
-      date,
-      content,
-    };
 
-    setTreatmentRecords((prev) => [...prev, newRecord]);
-    setImageFile(null);
-    setDate('');
-    setContent('');
-    setOpenModal(false);
-  }, [imageFile, date, content, treatmentRecords]);
-
-// 수정완료 버튼 클릭 시 데이터 수정
-const onEditRecord = useCallback(() => {
-  if (!selectedRecordIndex) {
-    alert('수정할 항목을 선택해주세요.');
-    return;
-  }
-
-  if (!imageFile || !date || !content) {
-    alert('모든 항목을 입력해주세요.');
-    return;
-  }
+    if (!selectedRecordIndex) {
+      alert('수정할 항목을 선택해주세요.');
+      return;
+    }
 
   const updatedRecord: TreatmentRecord = {
     ...treatmentRecords[selectedRecordIndex],
@@ -112,7 +160,7 @@ const onEditRecord = useCallback(() => {
     setOpenModal(false);
     }, [selectedRecordIndex, imageFile, date, content, treatmentRecords]);
 
-  // 취소버튼
+  // 모달 속 취소버튼
   const onCancelRecord = useCallback(() => {
     setImageFile(null);
     setDate('');
@@ -135,7 +183,7 @@ const onEditRecord = useCallback(() => {
   }, [selectedRecordIndex, treatmentRecords]);
 
   // 목록으로 이동하는 버튼
-  const handleBackButtonClick = () => {
+  const handleBackButton = () => {
     navigate(`/diary`);
   }
 
@@ -196,37 +244,38 @@ const onEditRecord = useCallback(() => {
         <div key={index} className={styles.record} onClick={() => onClickRecord(index)}>
           <div className={styles.recordDate}>{record.date}</div>
           <img className={styles.recordImage} src={record.image} alt="treatment" />
-          <div className={styles.recordIndex}>{record.index}</div>
+          <div className={styles.recordIndex}>{record.index}번째 치료일기</div>
           <div className={styles.recordContent}>
           {record.content.length > 10 ? `${record.content.substring(0, 10)}...` : record.content}
           </div>
         </div>
         ))}
+        
+        {/* 치료일지 생성, 목록보기 버튼 */}
         <div className={styles.detailButton}>
             <button className={styles.detailCreateButton} onClick={() => onClickCreateRecord()}>치료일지 생성</button>
-            <button className={styles.detailBackButton} onClick={()=>handleBackButtonClick()}>목록보기</button>
+            <button className={styles.detailBackButton} onClick={()=>handleBackButton()}>목록보기</button>
         </div>
       </div>
 
+      {/* 치료일지 모달들 */}
       {/* 카드를 눌렀을 때 모달 발생 */}
-      {/* 기부자가 클릭시에 확인 / 병원 계정으로 클릭 시에 수정, 삭제 isChecked*/}
+      {/* 기부자가 클릭시에 확인 / 병원 계정으로 클릭 시에 수정, 삭제 */}
+      {/* isChecked(병원계정 여부) 추가해야함 */}
       {isOpenModal && selectedRecordIndex !== null && (
       <Modal onClickToggleModal={onClickToggleModal}>
-        <div className={styles.modalTop}>
-        </div>
+        <div className={styles.modalTop}></div>
         <img src={treatmentRecords[selectedRecordIndex].image} alt="treatment" className={styles.modalImage} />
         <div className={styles.modalInfo}>
           <div className={styles.modalInfoTitle}>{treatmentRecords[selectedRecordIndex].index}</div>
           <hr className={styles.modalHr} />
           <div className={styles.modalInfoDate}>작성일 : {treatmentRecords[selectedRecordIndex].date}</div>
           <div className={styles.modalInfoContent}>{treatmentRecords[selectedRecordIndex].content}</div>
-          {!isChecked && (
             <div className={styles.modalButton}>
               <button className={styles.modalButtonItem} onClick={() => onEditRecord()}>수정</button>
               <button className={styles.modalButtonItem} onClick={() => onCancelRecord()}>취소</button>
               <button className={styles.modalButtonItem} onClick={() => onRemoveRecord()}>삭제</button>
             </div>
-          )}
           {isChecked && (
             <div className={styles.modalButton}>
               <button className={styles.modalButtonItem}>확인</button>
@@ -237,60 +286,40 @@ const onEditRecord = useCallback(() => {
       )}
       
       {/* 병원계정 -> 치료일지 생성 */}
-      {isOpenModal && (
-            <Modal onClickToggleModal={onClickToggleModal}>
-              <div className={styles.modalTop}>
-              <h2 className={styles.modalTitle}>병원 치료일지 생성</h2>
-              </div>
-            <form className={styles.modalForm}>
-              <div className={styles.modalFormRow}>
-                <label htmlFor="imageFile" className={styles.modalFormLabel}>
-                이미지 파일
-                </label>
-                <input
-                id="imageFile"
-                type="file"
-                accept="image/*"
-                onChange={(e) => setImageFile(e.target.files?.[0] || null)}
-                className={styles.modalFormInput}
-                />
-              </div>
-              <hr/>
-              <div className={styles.modalFormRow}>
-                <label htmlFor="date" className={styles.modalFormLabel}>
-                  날짜
-                </label>
-                <input
-                  id="date"
-                  type="date"
-                  value={date}
-                  onChange={(e) => setDate(e.target.value)}
-                  className={styles.modalFormInput}
-                />
-              </div>
-              <div className={styles.modalFormRow}>
-                <label htmlFor="content" className={styles.modalFormLabel}>
-                  내용
-                </label>
-                <textarea
-                  id="content"
-                  value={content}
-                  onChange={(e) => setContent(e.target.value)}
-                  className={styles.modalFormTextarea}
-                ></textarea>
-              </div>
-              </form>
-              <div className={styles.modalButton}>
-                <button className={styles.modalButtonItem} onClick={onAddRecord}>
-                  생성완료
-                </button>
-                <button className={styles.modalButtonItem} onClick={onCancelRecord}>
-                  취소
-                </button>
-              </div>
-            </Modal>
-            )}  
-      </div>
+      {isOpenModal && isChecked == true && (
+      <Modal onClickToggleModal={onClickToggleModal}>
+        <div className={styles.modalTop}></div>
+        <div className={styles.modalInfoTitle}>병원 치료일지 생성</div>
+        <div className={styles.modalInfo}> 
+          <SunEditor
+            getSunEditorInstance={getSunEditorInstance}
+                lang="ko"
+                width="300px"
+                height="300px"
+                autoFocus={false}
+                onChange={contentChangeHandler}
+                setDefaultStyle="font-family:Hahmlet;color:darkgrey;font-size: 20px;"
+                placeholder="환자의 치료일지를 적어주세요"
+                setOptions={{
+                    buttonList: [
+                        [
+                            "image",
+                        ]
+                            ]
+                        }}
+                    />
+        <div className={styles.modalButton}>
+          <button className={styles.modalButtonItem} onClick={onAddRecord}>
+              생성완료
+          </button>
+          <button className={styles.modalButtonItem} onClick={onCancelRecord}>
+              취소
+          </button>
+        </div>
+        </div>
+      </Modal>
+      )}       
+    </div>
   );
 };
 
