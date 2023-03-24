ff3d75e61d86e5fd4e63467928a81c086b7505b3
juggorr <juggorr@gmail.com>
2023-03-24 15:29:03 +0900

    :sparkles: [FE] 병원 페이지 모금목록 그리드 작업 중

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,12 +4,13 @@ import React from 'react';
 import './App.css';
 import MainLayout from "./components/MainLayout";
 // import Header from './components/header/Header';
-import Login from './pages/Login';
+// import Login from './pages/Login';
 
 // 병원 페이지
 import HospitalPage from "./pages/HospitalPage";
 import BenRegister from "./pages/BenRegister";
 import FundRegister from "./pages/FundRegister";
+import HosFundList from "./pages/HosFundList";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route element={<MainLayout />}>
             <Route path='/hospage' element={<HospitalPage />}/>
             <Route path='/benregi' element={<BenRegister />}/>
+            <Route path='/hosfundlist' element={<HosFundList />} />
             <Route path='/fundregi' element={<FundRegister />}/>
           </Route>
         </Routes>
