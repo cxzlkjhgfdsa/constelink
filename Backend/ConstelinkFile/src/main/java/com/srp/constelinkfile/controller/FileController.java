b42f920c9883084698fabfe8809a6615ee5c6632
LeeSH <mylifeforzug123@gmail.com>
2023-03-29 09:59:18 +0900

    :recycle: [BE] 스웨거 추가
    - 파일 서비스 스웨거 등록

--- Backend/ConstelinkFile/src/main/java/com/srp/constelinkfile/controller/FileController.java
+++ Backend/ConstelinkFile/src/main/java/com/srp/constelinkfile/controller/FileController.java
@@ -12,16 +12,21 @@ import org.springframework.web.multipart.MultipartFile;
 import com.srp.constelinkfile.dto.FileDto;
 import com.srp.constelinkfile.service.FileService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "파일", description = "파일 관련 api 입니다.")
 @RequestMapping("files")
 public class FileController {
 
 	private final FileService fileService;
+
+	@Operation(summary = "파일등록", description = "파일 등록 메서드.")
 	@PostMapping(value = "saveimg", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
 	public ResponseEntity uploadFile(@RequestParam("file") MultipartFile file){
 		FileDto upload = fileService.upload(file);
