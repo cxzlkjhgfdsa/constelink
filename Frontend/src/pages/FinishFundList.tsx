6b9eaf3800566d5350c917f89d061e1930ec3620
정원철 <gksrud316@naver.com>
2023-04-05 01:45:51 +0900

    :sparkles: [FE] 치료완성 페이지 시작
    - 카드제작, API 수정

--- Frontend/src/pages/FinishFundList.tsx
+++ Frontend/src/pages/FinishFundList.tsx
@@ -1,21 +1,69 @@
 import axios from 'axios';
-import React from 'react';
+import styles from './FinishFundList.module.css'
 import { useEffect, useState } from 'react';
 import { DonationData } from './../models/donatecard';
 
 const FinishFundList = () => {
-    const [finishList , setFinishList]=useState<DonationData[]>([]);
-    useEffect(()=>{
-        axios.get("fundraising/fundraisings?page=1&size=5&sortBy=FINISHED&memberId=0").then(res=>{
+    const [finishList, setFinishList] = useState<DonationData[]>([]);
+    useEffect(() => {
+        axios.get("/fundraising/fundraisings/withbeneficiaryinfo?page=1&size=5&sortBy=FINISHED&memberId=0").then(res => {
             console.log(res.data.content);
-            setFinishList(res.data.content)
+
+            setFinishList(res.data.content);
         })
-    } ,[])
+    }, [])
     return (
-        <div style={{paddingTop:"200px"}}>
-           {finishList.map((it,idx)=>{
-            return <div key={idx}>{it.categoryName}</div>
-           })}
+        <div className={styles.FinishFundList} >
+
+            <div className={styles.finish_title}>
+                Constelink로 연결된 인연
+            </div>
+
+           
+
+
+            <div className={styles.grid_section}>
+                {
+                    finishList.map((it, idx) => {
+                        return <div className={styles.grid_card} key={idx} style={{backgroundImage:`url(${it.fundraisingThumbnail})`,backgroundRepeat:"no-repeat", backgroundSize:"cover"}}>
+
+                            {/* <div className={styles.card_image} > */}
+                                {/* <img src={it.fundraisingThumbnail} alt="image"/> */}
+                            {/* </div> */}
+                            {/* <div className={styles.card_name}>
+                                <div className={styles.name}>{it.beneficiaryName}</div>
+                            </div> */}
+                            
+
+                            {/* <div className={styles.card_disease}>
+                                <div className={styles.disease}>{it.beneficiaryDisease}</div>
+                            </div> */}
+
+                            {/* <div className={styles.card_disease}>
+                                <div className={styles.disease}>{it.fundraisingPeople}</div>
+                            </div> */}
+
+
+                            <div className={styles.card_success}>
+
+                                <div className={styles.success}>Constelink</div>
+                            </div>
+
+                        </div>
+                    })
+
+                }
+
+
+
+
+            </div>
+
+
+
+
+
+
         </div>
     );
 };
