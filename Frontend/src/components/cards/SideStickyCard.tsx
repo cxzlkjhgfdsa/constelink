d2e95e9f21aa019f69054b18b7d17350250f9d39
gksrud316 <gksrud316@naver.com>
2023-04-05 17:23:48 +0900

    :lipstick: [FE] 홈페이지 광고 박스 설정

--- Frontend/src/components/cards/SideStickyCard.tsx
+++ Frontend/src/components/cards/SideStickyCard.tsx
@@ -1,4 +1,5 @@
 import styles from "./SideStickyCard.module.css";
+import { A11y } from 'swiper';
 interface Props {
   imageUrl: string;
 }
@@ -6,9 +7,26 @@ interface Props {
 const SideStickyCard: React.FC = () => {
   return (
     <div className={`${styles.sticky_box} `}>
-      <div className={styles.ad_image}>
+      <div className={styles.ad_box}>
           
-        
+        <div className={styles.box_top}>
+          <div className={styles.top_ment}>
+            
+          </div>
+           </div>
+
+        <div className={styles.box_bottom}>
+          
+        <div className={styles.bottom_ment}>
+            Constelink
+        </div>
+          
+          <div className={styles.dis_btn}>
+
+            <div className={styles.dis_button}>이동 ▶</div>
+          </div>
+          </div>
+
       </div>
     </div>
   );
