b1d42f49c36b1f48238097f7039e25e3e0373446
YoonDongGeun <ehdrmsdl9999@naver.com>
2023-04-05 23:56:01 +0900

    :sparkles: [FE] animation 대량 추가
    - 별 애니메이션 추가

--- Frontend/src/models/donatecard.ts
+++ Frontend/src/models/donatecard.ts
@@ -1,34 +1,33 @@
-
 export interface DonationData {
-  beneficiaryDisease:string;
+  beneficiaryDisease: string;
   beneficiaryName: string;
   beneficiaryPhoto: string;
-  beneficiaryId:number;
+  beneficiaryId: number;
   beneficiaryBirthday: number;
-  
-  categoryName:string;
-  beneficiaryStatus:string;
+
+  categoryName: string;
+  beneficiaryStatus: string;
   fundraisingAmountGoal: number;
   fundraisingAmountRaised: number;
-  fundraisingEndTime:number;
+  fundraisingEndTime: number;
   fundraisingIsDone: boolean;
   fundraisingBookmarked: boolean;
-  fundraisingPeople:number
+  fundraisingPeople: number;
   fundraisingStartTime: number;
   fundraisingStory: string;
   fundraisingThumbnail: string;
   fundraisingTitle: string;
   fundraisingId: number;
   photo: string;
+  hospitalName: string;
+  fundraisingWillUse: string;
 }
-
 
 export interface Statistics {
-  totalFundraisings: number,
-  totalFundraisingsFinished: number,
-  totalAmountedCash: number,
-  allDonation: number,
-  allMember: number,
-  allHospital: number,
+  totalFundraisings: number;
+  totalFundraisingsFinished: number;
+  totalAmountedCash: number;
+  allDonation: number;
+  allMember: number;
+  allHospital: number;
 }
-
--- Frontend/src/pages/FinishFundList.tsx
+++ Frontend/src/pages/FinishFundList.tsx
@@ -1,42 +1,46 @@
 import axios from "axios";
 import styles from "./FinishFundList.module.css";
-import { createElement, useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { DonationData } from "./../models/donatecard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 
+// .FinishFundList::before 여기에 전체 백그라운드 컬러
+// .raising_star1, .raising_star2 각각 애니메이션 delay가 다른 별
+// .star_box 별 담는 박스
+// @keyframe raisingStar 별 애니메이션
+// @keyframes card_star 카드 안의 별
+// @keyframes show 카드안의 환자관련 문구 올라가는 애니메이션
+// @keyframes hide 카드안의 Constelink 로고 사라지는 애니메이션
+
 const FinishFundList = () => {
   const [finishList, setFinishList] = useState<DonationData[]>([]);
-  const arr = Array(70);
-  const result = arr.map((num) => {
-    const el = document.createElement("div");
-    el.classList.add("my-class");
-    el.textContent = num;
-    return el;
-  });
-  const randomStars = useMemo<any>(
+
+  const randomStars = useMemo(
     () => (
-      <div>
-        {Array(70).map((item) => {
-          let a = document.createElement("div");
-          //   a.classList.add("raising_star");
-          a.style.right = (Math.random() * 256).toString();
-          a.style.top = (Math.random() * 256).toString();
-          return a;
-        })}
+      <div key="stars-1" className={styles.star_box}>
+        {Array(100)
+          .fill(null)
+          .map((item: null, index) => (
+            <div
+              key={`star-${index}`}
+              className={`${styles[`raising_star${index % 2}`]}`}
+              style={{
+                top: `${Math.random() * 1400 + 1700}px`,
+                right: `${Math.random() * 1460 + 40}px`,
+              }}
+            ></div>
+          ))}
       </div>
     ),
     []
   );
-
   useEffect(() => {
     axios
       .get(
         "/fundraising/fundraisings/withbeneficiaryinfo?page=1&size=5&sortBy=FINISHED&memberId=0"
       )
       .then((res) => {
-        console.log(res.data.content);
-
         setFinishList(res.data.content);
       });
   }, []);
@@ -64,24 +68,36 @@ const FinishFundList = () => {
                 <FontAwesomeIcon icon={faStar} />
               </div>
               <div className={styles.show_comment}>
-                <div>김떡국 환자</div>
-                <div>기부금 500만원으로 </div>
-                <div>떡국을 사먹고 </div>
-                <div>다 나으셨습니다</div>
-                <div>축하드립니다.</div>
+                <div>{it.beneficiaryName} 환자</div>
+                <div>
+                  {new Date(it.fundraisingEndTime).toLocaleDateString()}까지
+                  기부받은
+                </div>
+                <div>{it.fundraisingAmountRaised}원으로</div>
+                <div>
+                  {it.beneficiaryDisease}
+                  {(it.beneficiaryDisease.charCodeAt(
+                    it.beneficiaryDisease.length - 1
+                  ) -
+                    44032) %
+                    28 ===
+                  0
+                    ? "를"
+                    : "을"}{" "}
+                  {it.hospitalName}에서
+                </div>
+                <div> 치료하셨습니다.</div>
+                <div>치료 내역</div>
+                {it.fundraisingWillUse.split("/").map((item, index) => (
+                  <div key={`${it.fundraisingId}-${index}`}>{item}</div>
+                ))}
+                <div>후원자 : {it.fundraisingPeople}명</div>
               </div>
             </div>
           );
         })}
       </div>
       {randomStars}
-      <div className={`${styles.raising_star} ${styles.star1}`}></div>
-      <div className={`${styles.raising_star} ${styles.star2}`}></div>
-      <div className={`${styles.raising_star} ${styles.star1}`}></div>
-      <div className={`${styles.raising_star} ${styles.star1}`}></div>
-      <div className={`${styles.raising_star} ${styles.star2}`}></div>
-      <div className={`${styles.raising_star} ${styles.star1}`}></div>
-      <div className={`${styles.raising_star} ${styles.star1}`}></div>
     </div>
   );
 };
